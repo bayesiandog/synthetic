4080ddc1fe5b5db52b6baf7da601775a5a434b07
hakam00ra <epita73@gmail.com>
2023-12-10 13:24:07 +0200

    augment each object

--- synth.py
+++ synth.py
@@ -11,16 +11,15 @@ class Poly:
   def __init__(self, poly=None, name=None, age=None):
     self.name = name
     self.age = age
-    self.poly = np.array(poly, dtype=int)  
+    self.poly = np.array([poly], dtype=np.int32)
+    #self.poly = np.array(poly, dtype=int)  
     
 class WorkerSignals(QObject):
     connected = pyqtSignal(int)
 
 
 def getROI(image_array, poly):
-    # Black image except roi
-    
-    print(poly)
+    print(poly)    
     mask = np.ones_like(image_array, dtype=np.uint8) * 255
     cv.fillPoly(mask, poly, (0, 0, 0))
     resultWhite = cv.bitwise_or(image_array, mask)
@@ -56,7 +55,7 @@ class Synthesis(QMainWindow):
         self.img_ctr = 0
         self.setWindowTitle("Dataset Augmentation")
         self.setGeometry(100, 100, 2500, 1800)
-        
+        self.showMaximized()
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
 
@@ -81,11 +80,23 @@ class Synthesis(QMainWindow):
         self.button.clicked.connect(self.on_button_click)
         self.layout.addWidget(self.button)
 
+        # Add a button to save anotations
+        self.save_button = QPushButton("Save")
+        #self.save_button.clicked.connect(self.save)
+        self.layout.addWidget(self.save_button)
+        self.save_button.hide()
+
+        # Add a button to iterate through images in file
+        self.next_button = QPushButton("Next")
+        self.next_button.clicked.connect(self.iterate_images)
+        self.layout.addWidget(self.next_button)
+        self.next_button.hide()
+
          # Set up mouse events
         self.image_label.mousePressEvent = self.mouse_click
         self.image_label.mouseReleaseEvent = self.mouse_release
         self.image_label.mouseMoveEvent = self.mouse_move
-        self.polygon = []
+        
         
     def on_button_click(self):
         self.button.hide()
@@ -109,54 +120,60 @@ class Synthesis(QMainWindow):
         else:
             self.button.show()
 
-    def mouse_click(self, event):
-        global xp, yp
+    def mouse_click(self, event):        
         if event.button() == Qt.LeftButton:
             xp = event.x()
             yp = event.y()
+            self.xp = xp
+            self.yp = yp
             self.polygon.append((xp,yp))
-            LB = f'{xp}, {yp}'
-            cv.putText(self.image, LB, (xp, yp), cv.FONT_HERSHEY_PLAIN, 1, (255, 255, 0), 2)
-            q_image = QImage(self.image.data, self.width, self.height, self.bytes_per_line, QImage.Format_BGR888)
+            cv.circle(self.imageCopy, (xp, yp), radius=2, color=(255, 255, 0), thickness=-1)  # Thickness=-1 for filled circle
+            q_image = QImage(self.imageCopy.data, self.width, self.height, self.bytes_per_line, QImage.Format_BGR888)
             pixmap = QPixmap.fromImage(q_image)
             self.image_label.setPixmap(pixmap)            
     
     def mouse_move(self, event):
-        global xp, yp
         if event.buttons() & Qt.LeftButton:  # Check if the left button is pressed
             xp = event.x()
             yp = event.y()
-            self.polygon.append((xp, yp))  # Add the moved point to the list
-            LB = f'{xp}, {yp}'
-            #cv.putText(self.image, LB, (xp, yp), cv.FONT_HERSHEY_PLAIN, 1, (255, 255, 0), 2)
-            cv.circle(self.image, (xp, yp), radius=2, color=(255, 255, 0), thickness=-1)  # Thickness=-1 for filled circle
-            q_image = QImage(self.image.data, self.width, self.height, self.bytes_per_line, QImage.Format_BGR888)
+            self.polygon.append((xp, yp))  # Add the moved point to the list            
+            cv.circle(self.imageCopy, (xp, yp), radius=2, color=(255, 255, 0), thickness=-1)  # Thickness=-1 for filled circle
+            q_image = QImage(self.imageCopy.data, self.width, self.height, self.bytes_per_line, QImage.Format_BGR888)
             pixmap = QPixmap.fromImage(q_image)
             self.image_label.setPixmap(pixmap)
 
     def mouse_release(self, event):
-        global x, y
         if event.button() == Qt.LeftButton:
             x = event.x()
             y = event.y()
-            RB = f'{x}, {y}'
-            #cv.putText(self.image, RB, (x, y), cv.FONT_HERSHEY_PLAIN, 1, (0, 255, 255), 2)
-            cv.rectangle(self.image, (xp, yp), (x, y), (255, 0, 0), 2)
-            q_image = QImage(self.image.data, self.width, self.height, self.bytes_per_line, QImage.Format_BGR888)
+            self.objects.append(Poly(self.polygon))
+            print(self.objects[self.objCounter].poly)
+            maxX, maxY, minX, minY = self.get_boundaries(self.objects[self.objCounter].poly)
+            self.objCounter += 1
+            cv.rectangle(self.imageCopy, (minX, minY), (maxX, maxY), (255, 0, 0), 2)
+            q_image = QImage(self.imageCopy.data, self.width, self.height, self.bytes_per_line, QImage.Format_BGR888)
             pixmap = QPixmap.fromImage(q_image)
             self.image_label.setPixmap(pixmap)
+            self.polygon = []
             
 
-            image_array = np.array(self.image)
-            self.polygon = np.array([self.polygon], dtype=np.int32)
-            
-            
+    def iterate_images(self):
+        self.augment()
+        self.img_ctr += 1
+        if self.img_ctr < len(self.image_paths):
+            self.show_image(self.img_ctr)
 
-            roi = getROI(image_array, self.polygon)
-            moved = moveROI(image_array, 100, roi)
-            duplicate(image_array, self.polygon, 100, moved)
-
-            #self.anotation.show()
+    def augment(self):        
+        image_array = np.array(self.image)
+        #self.polygon = np.array([self.polygon], dtype=np.int32)
+        #self.get_boundaries(self.polygon)
+        roi = getROI(image_array, self.polygon)
+        moved = moveROI(image_array, 100, roi)
+        duplicate(image_array, self.polygon, 100, moved)
+        self.file_path = f"{self.image_paths[self.img_ctr].split('.jpg')[0]}"
+        self.file_path += "A.jpg"
+        print(self.file_path)
+        cv.imwrite(self.file_path, self.image)
 
     def show_logo(self, image_path):
         image = cv.imread(image_path)
@@ -168,25 +185,34 @@ class Synthesis(QMainWindow):
         self.image_label.setPixmap(pixmap)
         self.image_label.setAlignment(Qt.AlignCenter)
     
-    def show_image(self, counter):
+    def show_image(self, counter):                
         self.image = cv.imread(f'{self.image_paths[counter]}', cv.IMREAD_COLOR)
-        
+        self.imageCopy = self.image.copy()
         self.height, self.width, self.channel = self.image.shape
         label_size = self.image_label.size()
         
         if (self.height < label_size.height()) or self.width < label_size.width(): 
+            self.imageCopy = cv.resize(self.imageCopy, (label_size.width(), label_size.height()))
             self.image = cv.resize(self.image, (label_size.width(), label_size.height()))
 
-        self.height, self.width, self.channel = self.image.shape
+        self.height, self.width, self.channel = self.imageCopy.shape
         self.bytes_per_line = 3 * self.width
-        q_image = QImage(self.image.data, self.width, self.height, self.bytes_per_line, QImage.Format_BGR888)
+        q_image = QImage(self.imageCopy.data, self.width, self.height, self.bytes_per_line, QImage.Format_BGR888)
     
         pixmap = QPixmap.fromImage(q_image)
         self.image_label.setPixmap(pixmap)
-
         self.objects = []
+        self.objCounter = 0
+        self.polygon = []
+        self.next_button.show()
 
-
+    def get_boundaries(self, poly):
+        maxX = np.max(poly[:, :, 0])
+        maxY = np.max(poly[:, :, 1])
+        minX = np.min(poly[:, :, 0])
+        minY = np.min(poly[:, :, 1])
+        return maxX, maxY, minX, minY
+        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     synthesis = Synthesis()
